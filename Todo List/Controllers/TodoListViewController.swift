3a7aae85108e0bae040c7430a065b6424c5ac882
Brinit <brinittest@gmail.com>
2025-08-14 15:04:55 +0530

    Presiting Database using Codable Protocol and Plist

--- Todo List/Controllers/TodoListViewController.swift
+++ Todo List/Controllers/TodoListViewController.swift
@@ -8,22 +8,34 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
     
-    var listItems = ["pizza","Breads","Milk","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"]
-    let defaults = UserDefaults.standard
-
+    
+    var listItems = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
-//        if let item = defaults.array(forKey: "TodoList item") as? [String]{
-//            listItems = item
-//        }
-      
-      
-    }
 
+        
+        
+        print(dataFilePath)
+        let item1 = Item()
+        item1.title = "Milk"
+        listItems.append(item1)
+        
+        let item2 = Item()
+        item2.title = "Cake"
+        listItems.append(item2)
+        
+        let item3 = Item()
+        item3.title = "Bread"
+   
+        listItems.append(item3)
+        
+        loadItems()
+        
+    }
+    
     //MARK:- TableView DataSource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,11 +44,17 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "TodoItemCell")
-     //   let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = listItems[indexPath.row]
-       
+//        let cell = UITableViewCell(style: .default, reuseIdentifier: "TodoItemCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        let item = listItems[indexPath.row]
+        cell.textLabel?.text = item.title
       
+//        (!item.done) ? (cell.accessoryType = .none) : (cell.accessoryType = .checkmark)
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+       
         return cell
     }
     
@@ -44,42 +62,63 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else {
-                
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-      
+        listItems[indexPath.row].done = !listItems[indexPath.row].done
+        saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
-    //MARK:- Add new Items
-    
-    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
-        var addItemTextField = UITextField()
+
+        //MARK:- Add new Items
         
-        let alert = UIAlertController(title: "Add New TodoList Item", message: "", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            // What Happen if User Taped On Add on our UIAlert
-            if let textAvailable = addItemTextField.text{
-                if  textAvailable != "" {
-                    self.listItems.append(addItemTextField.text!)
-                    self.defaults.set(self.listItems, forKey: "TodoList item")
+        @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+            var addItemTextField = UITextField()
+            
+            let alert = UIAlertController(title: "Add New TodoList Item", message: "", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Add Item", style: .default) { [self] (action) in
+                // What Happen if User Taped On Add on our UIAlert
+                if let textAvailable = addItemTextField.text{
+                    if  textAvailable != ""{
+                        let item = Item()
+                        item.title =  addItemTextField.text!
+                        self.listItems.append(item)
+                        saveItems()
+                    }
                 }
+                
+                
+               
             }
-            
-            
-            self.tableView.reloadData()
+            alert.addTextField { (alertTextField) in
+                alertTextField.placeholder = "Create new item"
+                addItemTextField = alertTextField
+            }
+            alert.addAction(action)
+            present(alert, animated: true)
         }
-        alert.addTextField { (alertTextField) in
-            alertTextField.placeholder = "Create new item"
-            addItemTextField = alertTextField
-        }
-        alert.addAction(action)
-        present(alert, animated: true)
+
+    
+    func saveItems(){
         
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(listItems)
+            try data.write(to: dataFilePath!)
+        }catch {
+            print("Error encoding item array . \(error)")
+        }
+        self.tableView.reloadData()
     }
     
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+                listItems = try decoder.decode([Item].self, from: data)
+                
+            }catch{
+                print("Error Decode Item \(error)")
+            }
+        }
+    }
+    
+    
 }
-
--- Todo List/Item.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Item.swift
-//  Todo List
-//
-//  Created by Brinit on 13/08/25.
-//
-
-import Foundation
